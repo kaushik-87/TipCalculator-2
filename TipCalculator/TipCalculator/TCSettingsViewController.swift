502f6bb467124d19924c5ad5e605d81f61e1161f
Kaushik-87 <urskaush@gmail.com>
2017-07-23 17:38:21 -0700

    Renamed view controllers.

---
+++ TipCalculator/TipCalculator/TCSettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TipCalculator
+//
+//  Created by Dev on 7/23/17.
+//  Copyright © 2017 Kaushik. All rights reserved.
+//
+
+import UIKit
+
+class TCSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
