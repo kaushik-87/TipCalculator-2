1e707f79c994eca7286902c0e6a292f9aaf54fdf
Kaushik-87 <urskaush@gmail.com>
2017-08-06 10:29:16 -0700

    Adding more view elements.
    1. Stackviews for calculator buttons.
    2. Horizontal picker view for slipt bill.
    3. Slider for tip percentage.
    4. SegmentControl to switch between Tip percentage and split bill.

--- TipCalculator/TipCalculator/TCCalculatorViewController.swift
+++ TipCalculator/TipCalculator/TCCalculatorViewController.swift
@@ -8,10 +8,26 @@
 
 import UIKit
 
-class TCCalculatorViewController: UIViewController {
+class TCCalculatorViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    
+    
+    @IBOutlet weak var tipPercentageView:UIView!
+    @IBOutlet weak var splitBillView:UIView!
+    @IBOutlet weak var segmentControl:UISegmentedControl!
+    @IBOutlet weak var totalAmount:UILabel!
+    @IBOutlet weak var detailedLabel:UILabel!
+    @IBOutlet weak var splitByPicker:UIPickerView!
+    @IBOutlet weak var tipPercentageSlider:UISlider!
+    
+    var rotationAngle : CGFloat!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        rotationAngle = -50 * (.pi / 100)
+        splitByPicker.transform = CGAffineTransform(rotationAngle:rotationAngle)
+        splitByPicker.frame = CGRect(x:0, y:0, width:splitBillView.frame.width, height:splitBillView.frame.height)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +36,69 @@ class TCCalculatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func segmentSelection(sender: UISegmentedControl)
+    {
+        if (sender.selectedSegmentIndex == 0)
+        {
+            print("First")
+            self.splitBillView.isHidden = true
+            self.tipPercentageView.isHidden = false
+        }else
+        {
+            print("Second")
+            self.splitBillView.isHidden = false
+            self.tipPercentageView.isHidden = true
+        }
+    }
+    
+    // UIPicker delegate/ datasource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 20
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        return String(row)
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat
+    {
+        return 100
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView
+    {
+        var rotationAngle : CGFloat!
+        rotationAngle = 50 * (.pi / 100)
+        
+        if (view != nil) {
+            let label = UILabel(frame: CGRect(x: 0, y: 10, width: 70, height: 70))
+            label.text = String(row)
+            label.textAlignment = NSTextAlignment.center
+            label.textColor = UIColor.white
+            label.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            view?.addSubview(label)
+            return view!
+        }
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 110, height: 110))
+        let label = UILabel(frame: CGRect(x: 0, y: 10, width: 110, height: 110))
+        label.textColor = UIColor.white
+        label.textAlignment = NSTextAlignment.center
+        label.transform = CGAffineTransform(rotationAngle: rotationAngle)
+        label.text = String(row)
+        view.addSubview(label)
+        return view
+    }
+    
+    
+    
 
 }
 
